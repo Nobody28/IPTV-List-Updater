105246369629730b1f35523ba67422d6b48bb3ba
Nobody28 <Nobody28@hotmail.de>
2014-09-02 21:47:57 +0200

    Update Teledunet_id_update.sh

--- src/scripts/Teledunet_id_update.sh
+++ src/scripts/Teledunet_id_update.sh
@@ -7,23 +7,20 @@
 # 16.05.2014 testi/szudena: changes id0 to the old format
 # 23.05.2014 testi: fix ID0
 # 24.05.2014 szudena: changes E+13 to E+12 due to weppage changes
+# 02.09.2014 Nobody28: fix ID0
 #Get time_player
 #
 curl -c "teledunet.cks" "http://www.teledunet.com" > /dev/null 2>&1
 id0=`curl -A "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14" -b "teledunet.cks" -e "http://www.teledunet.com" "http://www.teledunet.com/mobile/?con" 2>/dev/null | tr -d '\r' | grep 'var aut='`
-id0=`echo $id0 | sed -e "s/var aut='?id0=//g" | sed -e "s/';//g" | sed -e "s/\n//g" | sed -e "s/\.//g" | sed -e "s/E+12/0/g"`
+#echo $id0
+
+id0=`echo $id0 | sed -e "s/var aut='?id0=//g" | sed -e "s/';//g" | sed -e "s/\n//g"`
+
 echo $id0
 #
 #now let`s update the id0 in the channellist
 #
-if [ -f /etc/enigma2/userbouquet.teledunet.tv ]
-	then
-	sed -i "s/id0=[0-9]\{13\}/id0="$id0/g /etc/enigma2/userbouquet.teledunet.tv
-fi
-if [ -f /etc/enigma2/userbouquet.ilu_teledunet.tv ]
-	then
-	sed -i "s/id0=[0-9]\{13\}/id0="$id0/g /etc/enigma2/userbouquet.ilu_teledunet.tv
-fi
+sed -i "s/id0=[0-9]\{12\}/id0="$id0/g /etc/enigma2/userbouquet.ilu_teledunet.tv
 #
 #now reload servicelist
 wget -q -O - http://127.0.0.1/web/servicelistreload?mode=2 > /dev/null 2>&1
